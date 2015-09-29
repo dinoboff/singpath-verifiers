40a7bead1360f464aa328bb86a29e7d511cd0a8f
Chris Boesch <cboesch@smu.edu.sg>
2015-09-29 14:49:20 +0800

    Adding notes on how to use containers for verification

--- test_all_verifiers.py
+++ test_all_verifiers.py
@@ -2,25 +2,27 @@
 # The initial development runs examples in a local directory until container execution can be implemented.  
 import json
 import os
+import sys
 
 """
-This script will need to be modified to write out problem.txt and solution.txt
-The individual lanuage verifier.py files will be responisble for any file renaming and test file building. 
-Loop through all of the examples and then find the needed files in each subfolder. 
-Add the ability to pass in a specific language key. (java, python, javascript, angularjs, pandas, spark)
-Make a loopback image as your first image whose verifier.py always writes out the same failing results.json
-
-Find a developer with docker experience who can update the docker image with one that works. 
-
 This script will mainly write out each problem and solution, run the docker verify command, collect the solution, 
 and then compare the results.
 You should be able to check all of the docker verifiers at once. 
 
-There can be 2 scripts - run all of these from subfolders on the local machine after being unzipped. 
-Then one to run all of these in the appropriate docker image. 
+There can be 2 scripts - run all of these from subfolders on the local machine. 
+Then one to run all of the problem in their docker containers. 
 """
 
-# Todo: pass in verifier directory
+run_in_containers = False
+
+if len(sys.argv) > 1 and sys.argv[1]=='run_in_containers':
+    print("Testing containers.")
+    run_in_containers = True
+
+docker_verifier_images = {}
+#docker_verifier_images['java']= ""
+#docker_verifier_images['python']= ""
+#docker_verifier_images['javascript']= ""
 
 # Write out tests from string
 def write_solution(solution_code,  directory):        
@@ -32,15 +34,32 @@ def write_test(test_code, directory):
         the_file.write(test_code)
         
 def run_secure_verifier(directory):
-    savedPath = os.getcwd()
-    os.chdir(directory)
-    os.system('python verify.py')
-    os.chdir(savedPath)
+    if run_in_containers:
+        print("Container execution still under development.")
+        # mount the directory to read problem.txt and solution.txt from. 
+        # docker run java_conatainer python verify.py
+        # read the results.json from container
+        # shutdown ccontainer
+        
+    else: 
+        # Test the verify.py scripts for each language in local subdirectories on a test system. 
+        savedPath = os.getcwd()
+        os.chdir(directory)
+        os.system('python verify.py')
+        os.chdir(savedPath)
     
 def read_results(directory):
-    with open(directory+'/results.json') as data_file:    
-        results = json.load(data_file)
-    return results
+    target = directory+'/results.json'
+    if os.path.exists(target):
+        with open(target) as data_file:    
+            results = json.load(data_file)
+        return results
+    else: 
+        # Write out a no results returned error. 
+        data = {"solved":False, 
+                "results": [["Verifier did not return a result", 0, compile_result, "fail"]]}
+        with open("results.json", 'w') as outfile:
+            json.dump(data, outfile) 
 
 # Load problem examples
 with open('problem_examples.json') as data_file:    
